8865382677b00869fe2a44f3a0d4e14b66dce548
Jeff Mattson <jeff@layer7web.com>
2018-01-21 15:09:44 -0500

    Footer identified.

--- page.tpl.php
+++ page.tpl.php
@@ -26,33 +26,69 @@
           <?php print render($page['featured']); ?>
         </div>
       </div>
-      <div id="center"><div id="squeeze"><div class="right-corner"><div class="left-corner">
-          <?php print $breadcrumb; ?>
-          <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-          <a id="main-content"></a>
-          <?php if ($tabs): ?><div id="tabs-wrapper" class="clearfix"><?php endif; ?>
-          <?php print render($title_prefix); ?>
-          <?php if ($title): ?>
-            <h1<?php print $tabs ? ' class="with-tabs"' : '' ?>><?php print $title ?></h1>
-          <?php endif; ?>
-          <?php print render($title_suffix); ?>
-          <?php if ($tabs): ?><?php print render($tabs); ?></div><?php endif; ?>
-          <?php print render($tabs2); ?>
-          <?php print $messages; ?>
-          <?php print render($page['help']); ?>
-          <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-          <div class="clearfix">
-            <?php print render($page['content']); ?>
-          </div>
-          <?php print $feed_icons ?>
-          <?php print render($page['footer']); ?>
-      </div></div></div></div> <!-- /.left-corner, /.right-corner, /#squeeze, /#center -->
-
+      <div class="row">
+        <div class="col-sm-12">
+          <div id="center">
+            <div id="squeeze">
+              <div class="right-corner">
+                <div class="left-corner">
+                  <?php print $breadcrumb; ?>
+                  <?php if ($page['highlighted']): ?>
+                  <div id="highlighted">
+                    <?php print render($page['highlighted']); ?>
+                  </div>
+                  <?php endif; ?>
+                  <a id="main-content"></a>
+                  <?php if ($tabs): ?>
+                  <div id="tabs-wrapper" class="clearfix">
+                    <?php endif; ?>
+                    <?php print render($title_prefix); ?>
+                    <?php if ($title): ?>
+                      <h1<?php print $tabs ? ' class="with-tabs"' : '' ?>><?php print $title ?></h1>
+                    <?php endif; ?>
+                    <?php print render($title_suffix); ?>
+                    <?php if ($tabs): ?><?php print render($tabs); ?>
+                  </div>
+                  <?php endif; ?>
+                  <?php print render($tabs2); ?>
+                  <?php print $messages; ?>
+                  <?php print render($page['help']); ?>
+                  <?php if ($action_links): ?>
+                    <ul class="action-links"><?php print render($action_links); ?></ul>
+                  <?php endif; ?>
+                  <div class="clearfix">
+                    <?php print render($page['content']); ?>
+                  </div>
+                  <?php print $feed_icons ?>
+                  <?php print render($page['footer']); ?>
+                </div>
+              </div>
+            </div>
+          </div> <!-- /.left-corner, /.right-corner, /#squeeze, /#center -->
+        </div>
+      </div> <!-- end row -->
       <?php if ($page['sidebar_second']): ?>
         <div id="sidebar-second" class="sidebar">
           <?php print render($page['sidebar_second']); ?>
         </div>
       <?php endif; ?>
+      <div id="footer-wrapper">
+        <div class="section">
+        <?php if ($page['footer_firstcolumn'] || $page['footer_secondcolumn'] || $page['footer_thirdcolumn'] || $page['footer_fourthcolumn']): ?>
+          <div id="footer-columns" class="clearfix">
+            <?php print render($page['footer_firstcolumn']); ?>
+            <?php print render($page['footer_secondcolumn']); ?>
+            <?php print render($page['footer_thirdcolumn']); ?>
+            <?php print render($page['footer_fourthcolumn']); ?>
+          </div> <!-- /#footer-columns -->
+        <?php endif; ?>
 
+        <?php if ($page['footer']): ?>
+          <div id="footer" class="clearfix">
+            <?php print render($page['footer']); ?>
+          </div> <!-- /#footer -->
+        <?php endif; ?>
+
+      </div></div> <!-- /.section, /#footer-wrapper -->
     </div> <!-- /#container -->
   </div> <!-- /#wrapper -->
