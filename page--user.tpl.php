cf4904a7429dfd0993d2014db0f58179a631cae0
Jeff Mattson <jeff@layer7web.com>
2018-02-03 11:29:43 -0500

    Created template-parts directory, footer include.

--- page--user.tpl.php
+++ page--user.tpl.php
@@ -40,33 +40,8 @@
           </div>
         <?php endif; ?>
       </div>
-      <div class="row">
-        <div class="col-sm-12">
-          <div id="footer-wrapper">
-            <div class="section">
-
-            <?php if ($page['footer_firstcolumn'] || $page['footer_secondcolumn'] || $page['footer_thirdcolumn'] || $page['footer_fourthcolumn']): ?>
-              <div id="footer-columns" class="clearfix">
-                <div class="row">
-                  <div class="col-md-3">
-                    <?php print render($page['footer_firstcolumn']); ?>
-                  </div>
-                  <div class="col-md-3">
-                    <?php print render($page['footer_secondcolumn']); ?>
-                  </div>
-                  <div class="col-md-3">
-                    <?php print render($page['footer_thirdcolumn']); ?>
-                  </div>
-                  <div class="col-md-3">
-                    <?php print render($page['footer_fourthcolumn']); ?>
-                  </div>
-                </div>
-              </div> <!-- /#footer-columns -->
-            <?php endif; ?>
-
-            </div>
-          </div> <!-- /.section, /#footer-wrapper -->
-        </div>
+      <?php include_once(drupal_get_path('theme',$GLOBALS['theme']).'/template-parts/footer.php'); ?>
       </div>
-    </div> <!-- /#container -->
-  </div> <!-- /#wrapper -->
+    </div>
+  </div> <!-- /#container -->
+</div> <!-- /#wrapper -->
--- page.tpl.php
+++ page.tpl.php
@@ -63,34 +63,8 @@
         </div>
       </div> <!-- end row -->
     </div>
-      <div class="row">
-        <div class="col-sm-12">
-          <div id="footer-wrapper">
-            <div class="section">
-
-            <?php if ($page['footer_firstcolumn'] || $page['footer_secondcolumn'] || $page['footer_thirdcolumn'] || $page['footer_fourthcolumn']) : ?>
-              <div id="footer-columns" class="clearfix">
-                <div class="row">
-                  <div class="col-md-3">
-                    <?php print render($page['footer_firstcolumn']); ?>
-                  </div>
-                  <div class="col-md-3">
-                    <?php print render($page['footer_secondcolumn']); ?>
-                  </div>
-                  <div class="col-md-3">
-                    <?php print render($page['footer_thirdcolumn']); ?>
-                  </div>
-                  <div class="col-md-3">
-                    <?php $loginBlock = drupal_get_form("search_block_form"); ?>
-                    <?php print(drupal_render($loginBlock)); ?>
-                  </div>
-                </div>
-              </div> <!-- /#footer-columns -->
-            <?php endif; ?>
-
-            </div>
-          </div> <!-- /.section, /#footer-wrapper -->
-        </div>
-      </div>
-    </div> <!-- /#container -->
-  </div> <!-- /#wrapper -->
+    <?php include_once(drupal_get_path('theme',$GLOBALS['theme']).'/template-parts/footer.php'); ?>
+    </div>
+  </div>
+</div> <!-- /#container -->
+</div> <!-- /#wrapper -->
